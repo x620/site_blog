5fde3fcc1caa0a47be83a330d170e5558442b784
jce <jce@yandex.ru>
2016-11-30 15:27:51 +0500

    Create blog when create user

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, Entry
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('user', 'name', 'id')
+	search_fields = ('name',)
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+	list_display = ('title', 'body', 'blog', 'pub_date', 'id')
+	search_fields = ('title', 'body')
--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,32 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+@receiver(post_save, sender=User)
+def create_user_blog(sender, instance, created, **kwargs):
+	if created and not instance.is_staff:
+		blog = Blog(user=instance)
+		blog.save()
+
+
+class Blog(models.Model):
+	name = models.CharField('Name of blog', max_length=100, blank=True, null=True)
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return 'Blog: %s' % self.user
+
+
+class Entry(models.Model):
+	blog = models.ForeignKey(Blog)
+	title = models.CharField('Title', max_length=255, blank=True, null=True)
+	body = models.TextField('Entry')
+	pub_date = models.DateField()
+
+	def __unicode__(self):
+		return '%s (author: %s)' % (self.title, self.blog.user)
